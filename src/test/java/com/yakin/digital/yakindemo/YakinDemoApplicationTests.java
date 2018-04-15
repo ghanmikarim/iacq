c9e97228d208bf3caedd823b359053656ef9bc39
Ghanmi <karim.ghanmi@gmail.com>
2018-04-15 15:19:07 +0200

    add comment

--- src/test/java/com/yakin/digital/yakindemo/YakinDemoApplicationTests.java
+++ src/test/java/com/yakin/digital/yakindemo/YakinDemoApplicationTests.java
@@ -10,6 +10,9 @@ import org.springframework.test.context.junit4.SpringRunner;
 public class YakinDemoApplicationTests {
 
 	@Test
+	/**
+	this method test the conntext load
+	*/
 	public void contextLoads() {
 	}
 
