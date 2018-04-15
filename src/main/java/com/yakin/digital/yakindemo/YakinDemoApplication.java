6f9633af10f624c65866ebf3ad90f9114c59b633
Ghanmi <karim.ghanmi@gmail.com>
2018-04-15 14:56:57 +0200

    add comment

--- src/main/java/com/yakin/digital/yakindemo/YakinDemoApplication.java
+++ src/main/java/com/yakin/digital/yakindemo/YakinDemoApplication.java
@@ -5,7 +5,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class YakinDemoApplication {
-
+/**
+création d'un application de test guit
+ */
 	public static void main(String[] args) {
 		SpringApplication.run(YakinDemoApplication.class, args);
 	}
