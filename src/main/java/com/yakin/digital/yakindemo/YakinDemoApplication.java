a94779d958dafa72c88fee236e7edb766add2e6f
Ghanmi <karim.ghanmi@gmail.com>
2018-04-15 14:31:00 +0200

    projet socle

---
+++ src/main/java/com/yakin/digital/yakindemo/YakinDemoApplication.java
@@ -0,0 +1,12 @@
+package com.yakin.digital.yakindemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YakinDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YakinDemoApplication.class, args);
+	}
+}
